d040645637bcb01f528d7fe7a265ba96459f34d6
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-09-01 11:24:55 -0400

    Add home route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db, Users
+from flask import Flask, render_template, redirect
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,9 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+
+@app.route('/')
+def home():
+    users = User.query.all()
+    return render_template("users.html", users = users)
