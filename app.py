1ae885a7fe5727221d55e57965caa503fbf9ae49
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-09-01 14:01:01 -0400

    Adding POST route for create user

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ def users():
 def adduser():
     return render_template('createuser.html')
 
+@app.route('/users/new', methods = ['POST'])
+def adduser():
+
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    imgurl = request.form['imgurl']
+
+    user = User(firstname = firstname, lastname = lastname, image_url = imgurl)
+
+    db.session.add(user)
+    db.session.commit()
+
+    users = User.query.all()
+
+    return redirect('/users.html', users = users)
+
 @app.route('/users/<int:userid>')
 def getuser(userid):
     user = User.query.filter_by(id = userid).first()
@@ -51,3 +67,30 @@ def getuser(userid):
 
     return redirect('/users')
 
+
+'''
+**GET */ :*** Redirect to list of users. (We’ll fix this in a later step).
+DONE
+
+**GET */users :*** Show all users. Make these links to view the detail page for the user. Have a link here to the add-user form.
+DONE
+
+**GET */users/new :*** Show an add form for users
+DONE
+
+**POST */users/new :*** Process the add form, adding a new user and going back to ***/users***
+TO-DO
+
+**GET */users/[user-id] :***Show information about the given user. Have a button to get to their edit page, and to delete the user.
+DONE
+
+**GET */users/[user-id]/edit :*** Show the edit page for a user. Have a cancel button that returns to the detail page for a user, and a save button that updates the user.
+DONE
+
+**POST */users/[user-id]/edit :***Process the edit form, returning the user to the ***/users*** page.
+DONE
+
+**POST */users/[user-id]/delete :*** Delete the user.
+TO-DO
+
+'''
