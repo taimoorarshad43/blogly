be24278d1e9cf72c8b3dc4216781477ff0b3bfdf
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-09-01 13:45:04 -0400

    Adding user/edit routing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -30,3 +30,24 @@ def adduser():
 def getuser(userid):
     user = User.query.filter_by(id = userid).first()
     return render_template('detail.html', user=user)
+
+@app.route('/users/<int:userid>/edit')
+def getuser(userid):
+    user = User.query.filter_by(id = userid).first()
+    return render_template('edituser.html', user=user)
+
+
+@app.route('/users/<int:userid>/edit', methods = ['POST'])
+def getuser(userid):
+
+    user = User.query.filter_by(id = userid).first()
+
+    user.firstname = request.form['firstname']
+    user.lastname = request.form['lastname']
+    user.image_url = request.form['imgurl']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
