53363fd6dd63d483edd3fc7e90876fdd3706be9f
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-09-01 12:37:12 -0400

    Adding user id route

--- app.py
+++ app.py
@@ -14,5 +14,19 @@ db.create_all()
 
 @app.route('/')
 def home():
-    users = User.query.all()
+    return redirect("/users")
+
+@app.route('/users')
+def users():
+    users = User.query.all()                                    # Will return a blank list for now
     return render_template("users.html", users = users)
+
+
+@app.route('/users/new')
+def adduser():
+    return render_template('createuser.html')
+
+@app.route('/users/<int:userid>')
+def getuser(userid):
+    user = User.query.filter_by(id = userid).first()
+    return render_template('detail.html', user=user)
